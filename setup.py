26194345cd58f0893db141c4d723e3f7962ef7c6
Trevor Heins <trevor.heins@stvinc.com>
2015-07-17 10:49:05 -0400

    Added upload and setup scripts

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='submit_cli-automator',
+      version='1.0',
+      install_requires=['paramiko >= 1.15.2', 'scp >= 0.10.2']
+)
---
+++ submit.py
@@ -0,0 +1,47 @@
+import paramiko
+import getpass
+import sys
+import os
+from scp import SCPClient
+
+try:
+    pathToUpload = sys.argv[1]
+except:
+    print 'No path to file to upload provided as first argument'
+    sys.exit(1)
+
+try:
+    tuxPathForFile = sys.argv[2]
+except:
+    print 'No tux path provided as second argument'
+    sys.exit(1)
+
+username = raw_input('Enter username: ')
+
+p = getpass.getpass()
+
+ssh = paramiko.SSHClient()
+
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+ssh.connect('tux.cs.drexel.edu', username=username, password=p)
+
+if(os.path.isdir(pathToUpload)):
+
+    scp = SCPClient(ssh.get_transport())
+    scp.put(pathToUpload, remote_path=tuxPathForFile, recursive=True)
+
+else:
+    scp = SCPClient(ssh.get_transport())
+    scp.put(pathToUpload, remote_path=tuxPathForFile, recursive=False)
+
+classAbbreviation= raw_input('Enter the class abbreviation: ')
+assignment = raw_input('Enter the assignment name: ')
+
+stdin, stdout, stderr = ssh.exec_command('cd {0}/{1}'.format(tuxPathForFile, tuxPathForFile.split('/')[-1]))
+print stdout.read()
+stdin, stdout, stderr = ssh.exec_command('submit_cli -c {0} -a {1} *'.format(classAbbreviation, assignment))
+print stdout.readlines()
+stdin, stdout, stderr = ssh.exec_command('submit_cli -c {0} --verify'.format(classAbbreviation, assignment))
+print stdout.readlines()
+ssh.close()
